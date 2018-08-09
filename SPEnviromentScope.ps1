dd22e456b0492d027075f9ac22436d9d305e6ec3
Toni Frankola <toni.frankola@gmail.com>
2018-08-09 15:39:52 +0200

    adding logic to ignore mysites

--- SPEnviromentScope.ps1
+++ SPEnviromentScope.ps1
@@ -1,6 +1,7 @@
 param (
     [switch]$loadAllItems = $false,
-    [switch]$includePersonalSites = $false
+    [switch]$includePersonalSites = $true,
+    [switch]$mySiteHostUrl = $null
  )
 
 if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"})-eq $null)
@@ -20,66 +21,79 @@ $outFile = "$(get-date -f yyyy-MM-dd-HH-mm-ss).csv"
 
 Write-Host -ForegroundColor Yellow "$(get-date -format g) - Using $outfile to log progress."
 
-"Web Application, Site Collection, Database Name, Size, Web Count, Lists Count, Items Count" | Out-File $outFile -Append
+"Web Application, My Site, Site Collection, Database Name, Size, Webs Count, Lists Count, Items Count" | Out-File $outFile -Append
 
 $WebApps = Get-SPWebApplication
 
+if($mySiteHostUrl -ne $nul -and -not ($mySiteHostUrl.endswith("/")))
+{
+        $mySiteHostUrl = $mySiteHostUrl+"/"
+}
+
 foreach ($WebApp in $WebApps)
 {
-
-    $Sites = Get-SPSite -WebApplication $WebApp -Limit All
-    
-    foreach($Site in $Sites)
-
+    if($mySiteHostUrl -ne $null)
     {
-
-        $SizeInKB = $Site.Usage.Storage
-        $SizeInGB = $SizeInKB/1024/1024/1024
-        $SizeInGB = [math]::Round($SizeInGB,2)
-
-        $webAppDisplayName = Get-StringHash $WebApp.DisplayName
-        $siteUrl = Get-StringHash $Site.URL
-        $contentDatabaseName = Get-StringHash $Site.ContentDatabase.Name
-
-        if($loadAllItems -eq $true)
-        {
-            $i=0
-            $websCount = 0;
-            $listsCount = 0;
-
-            Try
-            {
-                foreach ($SPWeb in $Site.AllWebs)
-                {
-                    foreach ($SPList in $SPWeb.Lists)
-                    {
-                       $i= $i + $SPList.ItemCount
-                       $listsCount++; 
-                    }
-
-                    $websCount++;
-                    $SPWeb.dispose() 
-                }
-            }
-            Catch
-            {
-                $i=-1;
-                $websCount = -1;
-                $listsCount = -1;
-            }
-        }
-        else
-        {
-            $i=-2;
-            $websCount = -2;
-            $listsCount = -2;
-        }
-
-        $Site.dispose()
-
-        $webAppDisplayName + "," + $siteUrl + "," + $contentDatabaseName + "," + $SizeInGB + "," + $websCount + "," + $listsCount + "," + $i | Out-File $outFile -Append
+        $isMySiteHost = $WebApp.Url -eq $mySiteHostUrl
+    }
+    else
+    {
+        $isMySiteHost = "Unknown"
     }
 
+    if($includePersonalSites -or ($isMySiteHost -eq "Unknown"))
+    {
+        $Sites = Get-SPSite -WebApplication $WebApp -Limit All
+        
+        foreach($Site in $Sites)
+        {
+            $SizeInKB = $Site.Usage.Storage
+            $SizeInGB = $SizeInKB/1024/1024/1024
+            $SizeInGB = [math]::Round($SizeInGB,2)
+
+            $webAppDisplayName = Get-StringHash $WebApp.DisplayName
+            $siteUrl = Get-StringHash $Site.URL
+            $contentDatabaseName = Get-StringHash $Site.ContentDatabase.Name
+
+            if($loadAllItems -eq $true)
+            {
+                $i=0
+                $websCount = 0;
+                $listsCount = 0;
+
+                Try
+                {
+                    foreach ($SPWeb in $Site.AllWebs)
+                    {
+                        foreach ($SPList in $SPWeb.Lists)
+                        {
+                        $i= $i + $SPList.ItemCount
+                        $listsCount++; 
+                        }
+
+                        $websCount++;
+                        $SPWeb.dispose() 
+                    }
+                }
+                Catch
+                {
+                    $i=-1;
+                    $websCount = -1;
+                    $listsCount = -1;
+                }
+            }
+            else
+            {
+                $i=-2;
+                $websCount = -2;
+                $listsCount = -2;
+            }
+
+            $Site.dispose()
+
+            $webAppDisplayName + "," + $isMySiteHost + "," + $siteUrl + "," + $contentDatabaseName + "," + $SizeInGB + "," + $websCount + "," + $listsCount + "," + $i | Out-File $outFile -Append
+        }        
+    }
 }
 
 Write-Host -ForegroundColor Green "$(get-date -format g) - Data writen to $outfile."
