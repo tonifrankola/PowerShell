485466be853fbf529b92623d8ac920740dbddce6
Toni Frankola <toni.frankola@gmail.com>
2018-08-10 13:58:45 +0200

    added RunWithElevatedPrivileges + error logging

--- SPEnviromentScope.ps1
+++ SPEnviromentScope.ps1
@@ -23,83 +23,88 @@ Write-Host -ForegroundColor Yellow "$(get-date -format g) - Using $outfile to lo
 
 "Web Application, My Site, Site Collection, Database Name, Size, Users Count, Webs Count, Lists Count, Items Count" | Out-File $outFile -Append
 
-$WebApps = Get-SPWebApplication
-
-
 if($mySiteHostUrl -ne $null -and -not ($mySiteHostUrl.endswith("/")))
 {
         $mySiteHostUrl = $mySiteHostUrl+"/"
 }
 
-foreach ($WebApp in $WebApps)
+[Microsoft.SharePoint.SPSecurity]::RunWithElevatedPrivileges(
 {
-    if($mySiteHostUrl -ne $null)
+    $WebApps = Get-SPWebApplication
+    foreach ($WebApp in $WebApps)
     {
-        $itemCountsMySiteHost = $WebApp.Url -eq $mySiteHostUrl
-    }
-    else
-    {
-        $itemCountsMySiteHost = "Unknown"
-    }
-
-    if($itemCountncludePersonalSites -or ($itemCountsMySiteHost -eq "Unknown"))
-    {
-        $Sites = Get-SPSite -WebApplication $WebApp -Limit All
-        
-        foreach($Site in $Sites)
+        if($mySiteHostUrl -ne $null)
         {
-            $SizeInKB = $Site.Usage.Storage
-            $SizeInGB = $SizeInKB/1024/1024/1024
-            $SizeInGB = [math]::Round($SizeInGB,2)
+            $itemCountsMySiteHost = $WebApp.Url -eq $mySiteHostUrl
+        }
+        else
+        {
+            $itemCountsMySiteHost = "Unknown"
+        }
 
-            $webAppDisplayName = Get-StringHash $WebApp.DisplayName
-            $siteUrl = Get-StringHash $Site.URL
-            $contentDatabaseName = Get-StringHash $Site.ContentDatabase.Name
-
-            if($loadAllItems -eq $true)
+        if($itemCountncludePersonalSites -or ($itemCountsMySiteHost -eq "Unknown"))
+        {
+            $Sites = Get-SPSite -WebApplication $WebApp -Limit All
+            
+            foreach($Site in $Sites)
             {
-                $userCount = 0;
-                $itemCount=0;
-                $websCount = 0;
-                $listsCount = 0;
+                $SizeInKB = $Site.Usage.Storage
+                $SizeInGB = $SizeInKB/1024/1024/1024
+                $SizeInGB = [math]::Round($SizeInGB,2)
 
-                Try
+                $webAppDisplayName = Get-StringHash $WebApp.DisplayName
+                $siteUrl = Get-StringHash $Site.URL
+                $contentDatabaseName = Get-StringHash $Site.ContentDatabase.Name
+
+                if($loadAllItems -eq $true)
                 {
-                    $userCount = $Site.RootWeb.AllUsers.Count
+                    $userCount = 0;
+                    $itemCount=0;
+                    $websCount = 0;
+                    $listsCount = 0;
 
-                    foreach ($SPWeb in $Site.AllWebs)
+                    Try
                     {
-                        foreach ($SPList in $SPWeb.Lists)
-                        {
-                        $itemCount= $itemCount + $SPList.ItemCount
-                        $listsCount++; 
-                        }
+                        $userCount = $Site.RootWeb.AllUsers.Count
 
-                        $websCount++;
-                        $SPWeb.dispose() 
+                        foreach ($SPWeb in $Site.AllWebs)
+                        {
+                            foreach ($SPList in $SPWeb.Lists)
+                            {
+                            $itemCount= $itemCount + $SPList.ItemCount
+                            $listsCount++; 
+                            }
+
+                            $websCount++;
+                            $SPWeb.dispose() 
+                        }
+                    }
+                    Catch
+                    {
+                        $ErrorMessage = $_.Exception.Message
+
+                        Write-Host "Error loading $Site.Url ($_.Exception.Message)." -ForegroundColor Yellow
+
+                        $userCount = -1;
+                        $itemCount=-1;
+                        $websCount = -1;
+                        $listsCount = -1;
                     }
                 }
-                Catch
+                else
                 {
-                    $userCount = -1;
-                    $itemCount=-1;
-                    $websCount = -1;
-                    $listsCount = -1;
+                    $userCount = -2;
+                    $itemCount=-2;
+                    $websCount = -2;
+                    $listsCount = -2;
                 }
-            }
-            else
-            {
-                $userCount = -2;
-                $itemCount=-2;
-                $websCount = -2;
-                $listsCount = -2;
-            }
 
-            $Site.dispose()
+                $Site.dispose()
 
-            $webAppDisplayName + "," + $itemCountsMySiteHost + "," + $siteUrl + "," + $contentDatabaseName + "," + $SizeInGB + "," + $userCount + "," + $websCount + "," + $listsCount + "," + $itemCount | Out-File $outFile -Append
-        }        
+                $webAppDisplayName + "," + $itemCountsMySiteHost + "," + $siteUrl + "," + $contentDatabaseName + "," + $SizeInGB + "," + $userCount + "," + $websCount + "," + $listsCount + "," + $itemCount | Out-File $outFile -Append
+            }        
+        }
     }
-}
+})
 
 Write-Host -ForegroundColor Green "$(get-date -format g) - Data writen to $outfile."
